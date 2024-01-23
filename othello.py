a707607caf06421e96cd876b0207a587c3e9d0c3
Niraj Karki <68698748+niraj-karki@users.noreply.github.com>
2024-01-23 18:17:05 -0500

    Update othello.py

--- othello.py
+++ othello.py
@@ -1,7 +1,7 @@
 class Othello(object):
     """Othello class implements the game logic of Othello board game."""
 
-    # Useful constants - refer to these class attributes as Othello.attributename
+    
     BLACK = 'B'     # access as Othello.BLACK
     WHITE = 'W'     # access as Othello.WHITE
     EMPTY = 'E'     # access as Othello.EMPTY
@@ -9,28 +9,8 @@ class Othello(object):
     PLAYER2 = 2     # access as Othello.PLAYER2
     TIE = 0         # access as Othello.TIE
 
-    # DO NOT MAKE CHANGES TO THIS METHOD
+    
     def __init__(self, board_size, start_player, start_player_disc):
-        """
-        Iniitializes Othello object's attributes as described below.
-
-        Parameters:
-            board_size: size of othello board
-            start_player: player that starts the game
-            start_player_disc: disc color of the start player
-
-        Instance Attributes:
-            self._size (int): initialized with the value of parameter board_size
-            self._turn (int): initialized with the value of parameter start_player
-            self._player1_disc (str): initialize it appropriately using the value of parameter start_player_disc
-            self._player2_disc (str): initialize it appropriately using the value of parameter start_player_disc
-            self._board (list of lists): game board initialized appropriately
-
-        Validations and Exceptions:
-            Raise ValueError if parameter board_size is not 4 or 6 or 8
-            Raise ValueError if parameter start_player is < 1 or > 2
-            Raise ValueError if parameter start_player_disc is not valid.
-        """
         if board_size < 4 or board_size > 8 or board_size % 2 != 0:
             raise ValueError("Board size can only be 4 or 6 or 8.")
         if start_player < Othello.PLAYER1 or start_player > Othello.PLAYER2:
@@ -61,7 +41,6 @@ class Othello(object):
 
 
     def get_board_size(self):
-        """Returns the board size."""
         return self._size
 
 
@@ -71,20 +50,10 @@ class Othello(object):
 
 
     def set_next_turn(self):
-        """Sets the value of self._turn to next player."""
         self._turn = Othello.PLAYER1 if self._turn == Othello.PLAYER2 else Othello.PLAYER2
 
 
     def get_player_disc(self, player):
-        """
-        Return the disc of player indicated by parameter player.
-
-        Parameter:
-            player (int): player number whose disc is returned
-
-        Validations and Exceptions:
-            Raise ValueError if parameter player is < 1 or > 2
-        """
         if player < Othello.PLAYER1 or player > Othello.PLAYER2:
             raise ValueError("Player number is not valid.")
         if player == Othello.PLAYER1:
@@ -94,18 +63,6 @@ class Othello(object):
 
 
     def is_valid_move(self, row, col, disc):
-        """
-        Return True if placing the disc at location row,col is valid; False otherwise.
-
-        Parameters:
-            row (int): row number (0-based index)
-            col (int): column number (0-based index)
-            disc (str): disc color
-       
-        Validations and Exceptions:
-            Return False if row and/or col values is not within the bounds of the board.
-            Raises ValueError if the value of parameter disc is not valid.
-        """
         if row < 0 or col < 0 or row > (self.get_board_size()-1) or col > (self.get_board_size()-1):
             return False
         if not (disc == Othello.BLACK or disc == Othello.WHITE):
@@ -133,14 +90,8 @@ class Othello(object):
         return False
 
     
-    # DO NOT MAKE CHANGES TO THIS METHOD
+    
     def is_a_valid_move_available(self, disc):
-        """
-        Return True if a valid move for disc is available; False otherwise.
-
-        Parameter:
-            disc (str): disc color
-        """
         # check if a valid move is available
         for i in range(self._size):
             for j in range(self._size):
@@ -150,17 +101,6 @@ class Othello(object):
 
 
     def place_disc_at_pos(self, row, col, disc):
-        """
-        Place the disc at location row,col on the board, if valid.
-
-        Parameters:
-            row (int): row number (0-index based)
-            col (int): column number (0-index based)
-            disc (str): disc color
-       
-        Validations and Exceptions:
-            Return None if placement of disc at location row,col is not valid (use is_valid_move() to check this).
-        """
         if self.is_valid_move(row, col, disc):
             self._board[row][col] = disc
             steps = [-1, 0, 1]
